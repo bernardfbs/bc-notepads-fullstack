b678dba6c7059e2b47eefa197ff33f571a27fdc0
Bernard Freire <bernardfbs@hotmail.com>
2023-12-30 15:03:12 -0300

    SQL

--- server/src/index.mjs
+++ server/src/index.mjs
@@ -18,11 +18,6 @@ function handleErrorMiddleware(err, req, res, next) {
   throw err;
 }
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
 app.use(express.json());
 app.use("/notepads", notepadController);
 app.use(handleErrorMiddleware);
